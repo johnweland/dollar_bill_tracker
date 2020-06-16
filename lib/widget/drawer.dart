147110d18cb4f00d4483883a00185ea8d2842ad0
John Weland <john.weland@gmail.com>
2020-06-16 14:29:18 -0700

    built app drawer to house settings, such as import/export (#2)
    Co-authored-by: John Weland <jweland@icsolutions.com>

---
+++ lib/widget/drawer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:dollar_bill_tracker/widget/drawerOptions.dart';
+
+class SettingsDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.green,
+                  Colors.greenAccent
+                ]
+              )
+            ),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Material(
+                    elevation: 10,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.settings,
+                        size: 80,
+                        color: Colors.green,
+                      ),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          DrawerOptions(Icons.restore, 'Import', ()=> {}),
+          DrawerOptions(Icons.backup, 'Export', ()=> {}),
+        ],
+      ),
+    );
+  }
+}
